2bf8a208b9d9fede491727b1392167749af8d795
Ricardogm2000 <rsgrijalva@espe.edu.ec>
2024-08-13 11:05:08 -0500

    Carpetas

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php include 'header.php';?>
+
+
+
+
+<!-- banner -->
+<div class="banner">    	   
+    <img src="images/photos/fondo.jpg"  class="img-responsive" alt="slide">
+    <div class="welcome-message">
+        <div class="wrap-info">
+            <div class="information">
+                <h1  class="animated fadeInDown">EcuaGuía: Escucha y vive un recorrido inolvidable.</h1>
+                <p class="animated fadeInUp">"Descubre una experiencia única al recorrer un museo con una audioguía detallada en tu idioma nativo."</p>                
+            </div>
+            <a href="#information" class="arrow-nav scroll wowload fadeInDownBig"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+</div>
+<!-- banner-->
+
+ 
+<!-- reservation-information -->
+<div id="information" class="spacer reserve-info ">
+<div class="container">
+<div class="row">
+<div class="col-sm-7 col-md-8">
+    <div class="embed-responsive embed-responsive-16by9 wowload fadeInLeft"><iframe  class="embed-responsive-item" src="//player.vimeo.com/video/55057393?title=0" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+</div>
+<div class="col-sm-5 col-md-4">
+<h3>EcuaGuía</h3>
+<p>EcuaGuía es una página web que ofrece audioguías detalladas para cada punto del museo en el idioma que elijas, permitiéndote vivir un recorrido único.</p>
+</div>
+</div>  
+</div>
+</div>
+<!-- reservation-information -->
+
+
+
+<!-- services -->
+<div class="spacer services wowload fadeInUp">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/Audio.jpg" class="img-responsive" alt="slide"></div>                
+                <div class="item  height-full"><img src="images/photos/Audio.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/Audio.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#RoomCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#RoomCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- audio guias-->
+            <div class="caption">Audioguías<a href="rooms-tariff.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="TourCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/MUSEO.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/MUSEO1.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/MUSEO2.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Museos Asociados<a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="FoodCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/espanol.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/ingles.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/ruso.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#FoodCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#FoodCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Idiomas<a href="gallery.php" class="pull-right"><i class="fa fa-edit"></i></a></div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- services -->
+
+
+<?php include 'footer.php';?>
